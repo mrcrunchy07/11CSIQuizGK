3f27c03b5f81a063bb7982ee672732776087c216
mrcrunchy07 <129013587+mrcrunchy07@users.noreply.github.com>
2023-03-30 14:21:24 +1300

    Create QuizGK_greeting.py

---
+++ QuizGK_greeting.py
@@ -0,0 +1,8 @@
+def instructions():
+  print("you will have 10 questions to answer from 4 choices. Type one choice you think is correct. Include the capitals aswell in your final answer.")
+
+print("Multi-choice General Knowledge Quiz")
+name = input("What is your name? ")
+print("Welcome",(name),)
+instructions()
+print("You may begin")
